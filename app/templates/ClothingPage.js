3a61b91eea8ab676cdaa5e8d89fae1358c02ac1c
Green Zero Development <admin@greenzero.us>
2023-09-10 19:42:04 -0400

    new gear section on clothing page

--- app/templates/ClothingPage.js
+++ app/templates/ClothingPage.js
@@ -29,7 +29,7 @@ const Content = styled.div`
         gap: 30px;
         max-width: 1280px;
         margin: 0 auto;
-        padding: 150px 8px 100px 8px;
+        padding: 150px 8px 50px 8px;
         .page-link {
             text-align: center;
             padding-bottom: 50px;
@@ -55,12 +55,70 @@ const Content = styled.div`
         }
         @media (min-width: 992px) {
             grid-template-columns: repeat(3, 1fr);
-            padding-bottom: 150px;
+            padding-bottom: 50px;
         }
     }
 `;
 
+const GearSection = styled.div`
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 50px;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 50px 8px 200px 8px;
+    text-align: center;
+    @media (min-width: 768px) {
+        grid-template-columns: repeat(2, 1fr);
+        padding: 100px 8px 150px 8px;
+        text-align: left;
+    }
+    .content {
+        position: relative;
+        order: 2;
+        margin: 80px auto 0 auto;
+        max-width: 500px;
+        @media (min-width: 768px) {
+            margin: 0px 0 0 0;
+        }
+        img {
+            position: absolute;
+            top: -100px;
+            left: -120px;
+        }
+        h2 {
+            position: relative;
+            font-size: 3rem;
+            padding-bottom: 15px;
+            text-transform: uppercase;
+            z-index: 2;
+        }
+        p {
+            position: relative;
+            font-size: 20px;
+            line-height: 1.6;
+            padding: 0 0 50px 0;
+            z-index: 2;
+        }
+    }
+    .image {
+        position: relative;
+        width: 100%;
+        height: 480px;
+        @media (min-width: 768px) {
+            order: 2;
+        }
+    }
+    .button {
+        position: relative;
+        z-index: 2;
+    }
+`;
+
 export default function ClothingPage({ pageData }) {
+
+    let gearButtonLink = getButtonLink(pageData.acf.andersons_gear_section.button.link_to_where, pageData.acf.andersons_gear_section.button.onsite_link, pageData.acf.andersons_gear_section.button.offsite_link, pageData.acf.andersons_gear_section.file_link);
 
     const keepBrowsing = pageData.global_sections[4];
     const productBoxes = pageData.global_sections[1];
@@ -86,6 +144,20 @@ export default function ClothingPage({ pageData }) {
                     })}
                 </div>
             </Content>
+
+            <GearSection>
+                <div className="content">
+                    <Image src={`${pageData.acf.andersons_gear_section.badge.url}`} alt={`${pageData.acf.andersons_gear_section.badge.alt}`} width={304} height={306} />
+                    <h2>{pageData.acf.andersons_gear_section.title}</h2>
+                    <p>{pageData.acf.andersons_gear_section.paragraph}</p>
+                    <a href={gearButtonLink}>
+                        <div className="green-button">{pageData.acf.andersons_gear_section.button.text}</div>
+                    </a>
+                </div>
+                <div className="image">
+                    <Image src={`${pageData.acf.andersons_gear_section.image.url}`} alt={`${pageData.acf.andersons_gear_section.image.alt}`} fill style={{ objectFit: 'contain' }} />
+                </div>
+            </GearSection>
 
             <KeepBrowsing keepBrowsing={keepBrowsing} />
 
