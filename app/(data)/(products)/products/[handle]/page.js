f263562284520c2a15f5e69a03880431e8e37688
Brendon Folsom <admin@greenzero.us>
2026-06-04 15:15:45 -0400

    Add Google-Ads.txt

--- app/(data)/(products)/products/[handle]/page.js
+++ app/(data)/(products)/products/[handle]/page.js
@@ -2,61 +2,112 @@ import { apiUrl, revalidateInterval } from '../../../../global-settings.js';
 import { notFound } from 'next/navigation';
 import ProductSingle from "../../../../templates/ProductSingle";
 
-async function getAllProducts() {
-  const res = await fetch(`https://andersons-general-store-statesboro.myshopify.com/products.json?limit=250`, {
+const SHOPIFY_STORE_URL = 'https://andersons-general-store-statesboro.myshopify.com';
+
+const ALLOWED_COLLECTION_HANDLES = ['hats', 'misc', 'tops'];
+
+async function shopifyFetch(url) {
+  const res = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
+
+      // This is not usually needed for public Shopify .json storefront endpoints.
+      // Keep it only if your setup requires it.
       'X-Shopify-Access-Token': process.env.SHOPIFY_REST_API_ACCESS_TOKEN,
     },
-    next: { revalidate: revalidateInterval }
+    next: { revalidate: revalidateInterval },
   });
+
   if (!res.ok) {
     throw Error(res.statusText);
-  } else {
-    return res.json();
   }
+
+  return res.json();
+}
+
+async function getProductsByCollection(collectionHandle) {
+  let page = 1;
+  let products = [];
+
+  while (true) {
+    const data = await shopifyFetch(
+        `${SHOPIFY_STORE_URL}/collections/${collectionHandle}/products.json?limit=250&page=${page}`
+    );
+
+    const pageProducts = data.products || [];
+    products = [...products, ...pageProducts];
+
+    if (pageProducts.length < 250) break;
+
+    page += 1;
+  }
+
+  return products;
+}
+
+async function getAllProducts() {
+  const collectionProductGroups = await Promise.all(
+      ALLOWED_COLLECTION_HANDLES.map((handle) => getProductsByCollection(handle))
+  );
+
+  const products = collectionProductGroups.flat();
+
+  // De-dupe products that may exist in multiple collections
+  const uniqueProducts = Array.from(
+      new Map(products.map((product) => [product.handle, product])).values()
+  );
+
+  return {
+    products: uniqueProducts,
+  };
 }
 
 async function getSingleProduct(handle) {
-    const res = await fetch(`https://andersons-general-store-statesboro.myshopify.com/products/${handle}.json/`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Shopify-Access-Token': process.env.SHOPIFY_REST_API_ACCESS_TOKEN,
-    },
-    next: { revalidate: revalidateInterval }
-    });
-      if (!res.ok) {
-        throw Error(res.statusText);
-      } else {
-        return res.json();
-      }
-  }
-
-export default async function Page({params: { handle } }) {
-
-  const _product = getSingleProduct(handle);
-  const product = await _product;
-
-  return (
-    <>
-      <ProductSingle productData={product.product} />
-    </>
+  const data = await shopifyFetch(
+      `${SHOPIFY_STORE_URL}/products/${handle}.json`
   );
 
-  
+  return data;
+}
+
+async function isAllowedProduct(handle) {
+  const products = await getAllProducts();
+  return products.products.some((product) => product.handle === handle);
+}
+
+export default async function Page({ params: { handle } }) {
+  const allowed = await isAllowedProduct(handle);
+
+  if (!allowed) {
+    notFound();
+  }
+
+  const product = await getSingleProduct(handle);
+
+  return (
+      <>
+        <ProductSingle productData={product.product} />
+      </>
+  );
 }
 
 export async function generateStaticParams() {
-  const _products = getAllProducts();
-  const products = await _products;
-  return products.products.map((productSing) => ({ 
-    handle: productSing.handle
+  const products = await getAllProducts();
+
+  return products.products.map((productSing) => ({
+    handle: productSing.handle,
   }));
 }
 
 export async function generateMetadata({ params: { handle } }) {
-  const _productSing = getSingleProduct(handle);
-  const productSing = await _productSing;
+  const allowed = await isAllowedProduct(handle);
+
+  if (!allowed) {
+    return {};
+  }
+
+  const productSing = await getSingleProduct(handle);
+
   return {
     title: productSing.product.title,
     description: productSing.product.body_html,
@@ -69,6 +120,6 @@ export async function generateMetadata({ params: { handle } }) {
     twitter: {
       title: productSing.product.title,
       description: productSing.product.body_html,
-    }
-  }
+    },
+  };
 }
